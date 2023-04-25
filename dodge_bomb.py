4beae76d611f5717cca1cf57129976c0dba110b4
yuhonma <c0a2213006@edu.teu.ac.jp>
2023-04-25 15:36:20 +0900

    練習問題5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -11,6 +11,23 @@ delta = {
         pg.K_RIGHT: (1,0)
         }
 
+
+def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面街を判定し、真理値タプルを返すような関数
+    引数1:画面SurfaceのRect
+    引数2:こうかとんまたは、爆弾SurfaceのRect
+    戻り値:横方向、縦方向のはみ出し判定結果(画面内:True / 画面外:Falce)
+    """
+
+    yoko, tate = True, True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -44,11 +61,22 @@ def main():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
 
+        if check_bound(screen.get_rect(), kk_rct) != (True,True):
+            for k, mv in delta.items():
+                if key_lst[k]:
+                    kk_rct.move_ip(-mv[0],-mv[1])
+
+
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct) 
 
         bb_rct.move_ip(vx,vy)
-        screen.blit(bb_img, bb_rct) #練習4
+        screen.blit(bb_img, bb_rct)
+        yoko,tate = check_bound(screen.get_rect(),bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
 
         pg.display.update()
         clock.tick(1000)
