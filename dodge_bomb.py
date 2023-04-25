ae5727b4279073658f4c8ffc9c8736d290c40de7
yuhonma <c0a2213006@edu.teu.ac.jp>
2023-04-25 14:31:57 +0900

    練習問題3 - 改の改

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,20 +10,18 @@ def main():
     bg_img = pg.image.load("./ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("./ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    tmr = 0
 
     bb_img = pg.Surface((20,20))
     pg.draw.circle(bb_img, (255,0,0), (10,10),10)
     bb_img.set_colorkey((0,0,0))
-
     x,y = random.randint(0,1600),random.randint(0,900)
-
-    screen.blit(bb_img, [x,y])
-
-    vx = +1
-    vy = +1
+    vx,vy = +1,+1
 
     bb_rct = bb_img.get_rect()
+    bb_rct.center = x,y
+
+
+    tmr = 0
 
     while True:
         for event in pg.event.get():
